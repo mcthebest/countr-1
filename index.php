6a929f076998f8612d7e98ec97a6aad75f118183
TheFabiCraft <44499040+thefabicraft-github@users.noreply.github.com>
2020-01-01 21:24:17 +0100

    Removed unneccesary function

--- index.php
+++ index.php
@@ -101,29 +101,6 @@ if(session('access_token')) {
 		</a></div>';
 }
 
-
-function apiRequest($url, $post=FALSE, $headers=array()) {
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-    $response = curl_exec($ch);
-
-
-    if($post)
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-
-    $headers[] = 'Accept: application/json';
-
-    if(session('access_token'))
-        $headers[] = 'Authorization: Bearer ' . session('access_token');
-
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-    $response = curl_exec($ch);
-    return json_decode($response);
-}
-
 function get($key, $default=NULL) {
     return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
 }
